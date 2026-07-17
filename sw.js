6ca095117042a92dd31c926eac1e656fc4464ca2
Pathan Diya <180064234+Pathan-coder@users.noreply.github.com>
2026-07-17 22:36:16 +0530

    Fix case of index.html in cache list

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "/",
-        "/Index.html",
+        "/index.html",
         "/style.css",
         "/script.js",
         "/manifest.json"
