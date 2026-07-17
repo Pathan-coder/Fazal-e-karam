1263621b5fe4d9c4a1dff60eb15162917bd6b4ad
Pathan Diya <180064234+Pathan-coder@users.noreply.github.com>
2026-07-17 22:28:48 +0530

    Register service worker in script.js

--- script.js
+++ script.js
@@ -345,3 +345,8 @@ onSnapshot(doc(db, "azaanBookings", today), (snap) => {
   });
 
 });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch(err => console.log(err));
+                              }
