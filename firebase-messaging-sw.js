c3f72c0016768b540a22b6e775527b51191cfba8
Pathan Diya <180064234+Pathan-coder@users.noreply.github.com>
2026-07-19 22:03:05 +0530

    Update Firebase config and add background message handling
    Updated Firebase configuration with actual API key and app ID. Added background message handling to show notifications.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,24 @@
+
 importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCqrrsFXVqgVDGsO-JTZiKa2N8lzDBqkMO",
   authDomain: "fazal-e-karm.firebaseapp.com",
   projectId: "fazal-e-karm",
   storageBucket: "fazal-e-karm.firebasestorage.app",
   messagingSenderId: "768507616623",
-  appId: "YOUR_APP_ID"
+  appId: "1:768507616623:web:c0a43a292762d814905c9a"
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/3859.png"
+    }
+  );
+});
