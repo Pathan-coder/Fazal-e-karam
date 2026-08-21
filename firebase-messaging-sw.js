20696116a167ec5d3d263e4eadb144a92d44fa55
Pathan Diya <180064234+Pathan-coder@users.noreply.github.com>
2026-08-21 20:36:58 +0530

    Change Firebase API key in firebase-messaging-sw.js
    Updated the Firebase API key in the service worker.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCqrrsFXVqgVDGsO-JTZiKa2N8lzDBqkMO",
+  apiKey: "AIzaSyCqrrsFXVqgVDGsO-JTZiKa2N8lzDBqkM0",
   authDomain: "fazal-e-karm.firebaseapp.com",
   projectId: "fazal-e-karm",
   storageBucket: "fazal-e-karm.firebasestorage.app",
