4299fac4e31d81e9091a6256de9bfd2a1feaa9af
Pathan Diya <180064234+Pathan-coder@users.noreply.github.com>
2026-08-22 09:02:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,53 +1,39 @@
 const CACHE_NAME = "fazal-v3";
 
 const urlsToCache = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
-    "./3859.png",
-    "./3857.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./3859.png",
+  "./3857.png"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
-
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
-
-    self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                const copy = response.clone();
-
-                caches.open(CACHE_NAME).then(cache => {
-                    cache.put(event.request, copy);
-                });
-
-                return response;
-            })
-            .catch(() => {
-                return caches.match(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
